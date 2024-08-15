53de6f38f96da59f736e9720f0cd15b528589827
RaihanAnthony <raihanmalay14@gmail.com>
2024-08-15 17:48:37 +0700

    updated database.go

--- database.go
+++ database.go
@@ -13,23 +13,34 @@ var (
 )
 
 func DB_Connection() {
-	// var (
-	// 	dbUser = os.Getenv("DB_USER")
-	// 	dbPwdd = os.Getenv("DB_PASSWORD")
-	// 	dbHost = os.Getenv("DB_HOST")
-	// 	dbPort = "3306"
-	// 	dbName = os.Getenv("DB_NAME")
-	// )
+	var (
+		dbUser string
+		dbPwdd string
+		dbHost  string
+		dbName string
+		dbPort = "3306"
+	)
 
-	// dbUser := "root"
-	// dbPwdd := "0987"
-	// dbHost := "localhost"
-	// dbPort := "3306"
-	// dbName := "TB_Berkah_Jaya"
+	dbUser = os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "root"
+	}
+	dbPwdd = os.Getenv("DB_PASSWORD")
+	if dbPwdd == "" {
+		dbPwdd = "90909090"
+	}
+	dbHost = os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "/cloudsql/api-tb-berkah-jaya:us-central1:db-tb-berkah-jaya21"
+	}
+	dbName = os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "db_tb_berkah_jaya"
+	}
 
-	// dbURI := fmt.Sprintf("root:0987@tcp(localhost:3306)/TB_Berkah_Jaya?parseTime=true", dbUser, dbPwdd, dbHost, dbPort, dbName)
+	dbURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPwdd, dbHost, dbPort, dbName)
 
-	db, err := gorm.Open(mysql.Open("root:0987@tcp(localhost:3306)/TB_Berkah_Jaya?parseTime=true"))
+	db, err := gorm.Open(mysql.Open(dbURI))
 	if err != nil {
 		panic(err)
 	}
